6f8a98b828ef073bdac38708cc7f20a79bbb30b0
Kevin <kevinpang@master-dynamic.com>
2017-09-28 13:39:07 +0800

    Updated project to latest editor version

--- Assets/CellBoundaryController.cs
+++ Assets/CellBoundaryController.cs
@@ -19,10 +19,10 @@ public class CellBoundaryController : MonoBehaviour {
 
     private void Init()
     {
-        boundaries[0] = gameObject.transform.FindChild("NorthBoundary").GetComponent<BoxCollider2D>();
-        boundaries[1] = gameObject.transform.FindChild("EastBoundary").GetComponent<BoxCollider2D>();
-        boundaries[2] = gameObject.transform.FindChild("SouthBoundary").GetComponent<BoxCollider2D>();
-        boundaries[3] = gameObject.transform.FindChild("WestBoundary").GetComponent<BoxCollider2D>();
+        boundaries[0] = gameObject.transform.Find("NorthBoundary").GetComponent<BoxCollider2D>();
+        boundaries[1] = gameObject.transform.Find("EastBoundary").GetComponent<BoxCollider2D>();
+        boundaries[2] = gameObject.transform.Find("SouthBoundary").GetComponent<BoxCollider2D>();
+        boundaries[3] = gameObject.transform.Find("WestBoundary").GetComponent<BoxCollider2D>();
 
     }
 
